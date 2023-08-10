6262db5a9e9280b777c60d710be07486ab82a691
Vaishnav Barbade <76620267+VaishnavPatil@users.noreply.github.com>
2023-08-10 22:28:25 +0530

    Add files via upload

---
+++ Kth smallest element.cpp
@@ -0,0 +1,257 @@
+//
+// Created by vaishnav on 10-08-2023.
+//
+
+//Problem Statement
+/*
+ Given an array arr[] and an integer K where K is smaller than size of array, the task is to find the Kth smallest element in the given array. It is given that all array elements are distinct.
+
+Note :-  l and r denotes the starting and ending index of the array.
+
+Example 1:
+
+Input:
+N = 6
+arr[] = 7 10 4 3 20 15
+K = 3
+Output : 7
+Explanation :
+3rd smallest element in the given
+array is 7.
+Example 2:
+
+Input:
+N = 5
+arr[] = 7 10 4 20 15
+K = 4
+Output : 15
+Explanation :
+4th smallest element in the given
+array is 15.
+Your Task:
+You don't have to read input or print anything. Your task is to complete the function kthSmallest() which takes the array arr[], integers l and r denoting the starting and ending index of the array and an integer K as input and returns the Kth smallest element.
+
+
+Expected Time Complexity: O(n)
+Expected Auxiliary Space: O(log(n))
+Constraints:
+1 <= N <= 105
+1 <= arr[i] <= 105
+1 <= K <= N
+
+ */
+
+/*                     -------------------EDITORIAL---------------------------
+        // C++ implementation of worst case linear time algorithm
+// to find k'th smallest element
+#include<iostream>
+#include<algorithm>
+#include<climits>
+
+using namespace std;
+
+int partition(int arr[], int l, int r, int k);
+
+// A simple function to find median of arr[].  This is called
+// only for an array of size 5 in this program.
+int findMedian(int arr[], int n)
+{
+    sort(arr, arr+n);  // Sort the array
+    return arr[n/2];   // Return middle element
+}
+
+// Returns k'th smallest element in arr[l..r] in worst case
+// linear time. ASSUMPTION: ALL ELEMENTS IN ARR[] ARE DISTINCT
+int kthSmallest(int arr[], int l, int r, int k)
+{
+    // If k is smaller than number of elements in array
+    if (k > 0 && k <= r - l + 1)
+    {
+        int n = r-l+1; // Number of elements in arr[l..r]
+
+        // Divide arr[] in groups of size 5, calculate median
+        // of every group and store it in median[] array.
+        int i, median[(n+4)/5]; // There will be floor((n+4)/5) groups;
+        for (i=0; i<n/5; i++)
+            median[i] = findMedian(arr+l+i*5, 5);
+        if (i*5 < n) //For last group with less than 5 elements
+        {
+            median[i] = findMedian(arr+l+i*5, n%5);
+            i++;
+        }
+
+        // Find median of all medians using recursive call.
+        // If median[] has only one element, then no need
+        // of recursive call
+        int medOfMed = (i == 1)? median[i-1]:
+                                 kthSmallest(median, 0, i-1, i/2);
+
+        // Partition the array around a random element and
+        // get position of pivot element in sorted array
+        int pos = partition(arr, l, r, medOfMed);
+
+        // If position is same as k
+        if (pos-l == k-1)
+            return arr[pos];
+        if (pos-l > k-1)  // If position is more, recur for left
+            return kthSmallest(arr, l, pos-1, k);
+
+        // Else recur for right subarray
+        return kthSmallest(arr, pos+1, r, k-pos+l-1);
+    }
+
+    // If k is more than number of elements in array
+    return INT_MAX;
+}
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// It searches for x in arr[l..r], and partitions the array
+// around x.
+int partition(int arr[], int l, int r, int x)
+{
+    // Search for x in arr[l..r] and move it to end
+    int i;
+    for (i=l; i<r; i++)
+        if (arr[i] == x)
+           break;
+    swap(&arr[i], &arr[r]);
+
+    // Standard partition algorithm
+    i = l;
+    for (int j = l; j <= r - 1; j++)
+    {
+        if (arr[j] <= x)
+        {
+            swap(&arr[i], &arr[j]);
+            i++;
+        }
+    }
+    swap(&arr[i], &arr[r]);
+    return i;
+}
+
+// Driver program to test above methods
+int main()
+{
+    int arr[] = {12, 3, 5, 7, 4, 19, 26};
+    int n = sizeof(arr)/sizeof(arr[0]), k = 3;
+    cout << "K'th smallest element is "
+         << kthSmallest(arr, 0, n-1, k);
+    return 0;
+}
+
+ */
+
+
+/*  -------------------------------Authors Solution-----------------------------------------
+        //Back-end complete function template for C++
+
+class Solution{
+    public:
+    void swap(int *a, int *b)
+    {
+        int temp = *a;
+        *a = *b;
+        *b = temp;
+    }
+    // finding the pivot element and partition the array along that
+    int randomPartition(int arr[], int l, int r)
+    {
+        int n = r-l+1;
+        int pivot = rand() % n;
+        swap(&arr[l + pivot], &arr[r]);
+        return partition(arr, l, r);
+    }
+    int kthSmallest(int arr[], int l, int r, int k)
+    {
+        // If k is smaller than number of elements in array
+        if (k > 0 && k <= r - l + 1)
+        {
+            // find a position for random partition
+            int pos = randomPartition(arr, l, r);
+
+            // if this pos gives the kth smallest element, then return
+            if (pos-l == k-1)
+                return arr[pos];
+
+            // else recurse for the left and right half accordingly
+            if (pos-l > k-1)
+                return kthSmallest(arr, l, pos-1, k);
+            return kthSmallest(arr, pos+1, r, k-pos+l-1);
+        }
+
+        return INT_MAX;
+    }
+
+    // partitioning the array along the pivot
+    int partition(int arr[], int l, int r)
+    {
+        int x = arr[r], i = l;
+        for (int j = l; j <= r - 1; j++)
+        {
+            if (arr[j] <= x)
+            {
+                swap(&arr[i], &arr[j]);
+                i++;
+            }
+        }
+        swap(&arr[i], &arr[r]);
+        return i;
+    }
+
+};
+
+
+ */
+/*--------------------------------------MY SOLUTION------------------------------------------*/
+////{ Driver Code Starts
+//Initial function template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[], int l, int r, int k)
+    {
+        std::nth_element(arr + l, arr + l + k - 1, arr + r + 1);
+        return arr[l + k - 1];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int test_case;
+    cin>>test_case;
+    while(test_case--)
+    {
+        int number_of_elements;
+        cin>>number_of_elements;
+        int a[number_of_elements];
+
+        for(int i=0;i<number_of_elements;i++)
+            cin>>a[i];
+
+        int k;
+        cin>>k;
+        Solution ob;
+        cout<<ob.kthSmallest(a, 0, number_of_elements-1, k)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
